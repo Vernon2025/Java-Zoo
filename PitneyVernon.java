df5b1c06ef8db824ff466ad0b8a6632abce61059
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2023-01-19 06:18:57 -0800

    Done

---
+++ PitneyVernon.java
@@ -0,0 +1,29 @@
+
+/**
+ * A BoopleSnoot is Mr. Jaffe's animal
+ *
+ * @author Mr. Jaffe
+ * @version 1.0 2017-07-14
+ */
+public class PitneyVernon extends Animal
+{
+    public PitneyVernon() {
+        super("PitneyVernon", "Blue", 4);
+    }
+
+    public String speak() {
+        return "Humming";
+    }
+
+    public String getDisposition() {
+        return "Angry";
+    }
+
+    public String getType() {
+        return "Llama";
+    }
+
+    public String getName() {
+        return "Major Mayo";
+    }
+}
